67c27ba7a22068ca550a306f3e4199a71138064e
Maskul96 <maskulaniskamil96@gmail.com>
2024-05-31 18:51:24 +0200

    Dodanie interfejsu IEntity do klasy EntityBase

--- HMIApp/HMIApp/Components/DataBaseModels/Entities/EntityBase.cs
+++ HMIApp/HMIApp/Components/DataBaseModels/Entities/EntityBase.cs
@@ -1,7 +1,9 @@
-﻿namespace HMIApp.Components.DataBase.Entities
+﻿using HMIApp.Components.DataBaseModels.Entities;
+
+namespace HMIApp.Components.DataBase.Entities
 {
     //Klasa abstrakcyjna jako baza do dziedziczenia
-    public abstract class EntityBase
+    public abstract class EntityBase :IEntity
     {
         public int Id { get; set; }
     }
---
+++ HMIApp/HMIApp/Components/DataBaseModels/Entities/IEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace HMIApp.Components.DataBaseModels.Entities
+{
+   public interface IEntity
+    {
+        int Id { get; set; }
+    }
+}
